e1659df5efba7cbe9be92e96daa66072a74e7780
savioigt@gmail.com <savioigt@gmail.com@1ba91d2d-7f6b-f8f7-b0d8-db0b495ed80a>
2014-11-02 14:31:15 +0000

    Classe Resource Compra

--- Servico/ServicoVendedores/src/ufc/sd/resource/CompraResource.java
+++ Servico/ServicoVendedores/src/ufc/sd/resource/CompraResource.java
@@ -1,5 +1,22 @@
 package ufc.sd.resource;
 
-public class CompraResource {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
+import ufc.sd.controller.CompraController;
+import ufc.sd.modelo.Compra;
+
+@Path("/compra")
+public class CompraResource {
+	@POST
+	@Path("/nova")
+	@Produces("text/plain")
+	@Consumes("application/json")
+	public String cadastrarCompra(Compra compra){
+		return new CompraController().cadastrarCompra(compra);
+	}
+	
+	
 }
